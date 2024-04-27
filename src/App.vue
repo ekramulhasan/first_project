abb2f29349ec7b0e2a35e341fb1259de5a3bc398
ekramul <ekramulshawon1@gmail.com>
2024-04-27 16:20:33 +0600

    Teleport

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,25 @@
 <h1>{{ title }}</h1>
 
 <input type="text" ref="input">
+<br>
+<br>
+
+<div class="card">
+
+  <button @click="modal = true">show modal</button>
+
+ <Teleport to="body">
+
+  <div v-if="modal" >
+  <p>hello world, this this modal here</p>
+  <button @click="modal = false">hide modal</button>
+  </div>
+
+ </Teleport>
+
+</div>
+
+
  
 </template>
 
@@ -12,9 +31,13 @@ import { onBeforeMount,ref,onMounted } from 'vue';
 let title = ref('new item avaiable');
 const input = ref(null);
 
+let modal = ref(false);
+
 onMounted(() => {
   input.value.focus()
   console.log(input.value);
 });
+
+
 
 </script>
