579ced6def22738a9e98d47d373fd73a35f2871d
ekramul <ekramulshawon1@gmail.com>
2024-04-27 17:31:21 +0600

    make component and include

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,15 @@
 
 <h1>{{ title }}</h1>
 
-<input type="text" ref="input">
-<br>
-<br>
-
-<div class="card">
-
-  <button @click="modal = true">show modal</button>
-
- <Teleport to="body">
-
-  <div v-if="modal" >
-  <p>hello world, this this modal here</p>
-  <button @click="modal = false">hide modal</button>
-  </div>
-
- </Teleport>
-
-</div>
-
-
+<Counter />
  
 </template>
 
 <script setup>
-import { onBeforeMount,ref,onMounted } from 'vue';
+import { ref } from 'vue';
+import Counter from './components/Counter.vue';
 
 let title = ref('new item avaiable');
-const input = ref(null);
-
-let modal = ref(false);
-
-onMounted(() => {
-  input.value.focus()
-  console.log(input.value);
-});
-
-
 
 </script>
+
---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+   
+<div class="">
+
+<button @click="count++">+</button>
+<p>{{ count }}</p>
+<button @click="count--">-</button>
+
+</div>
+
+</template>
+
+<script setup>
+import { ref} from 'vue';
+
+let count = ref(0);
+
+
+</script>
